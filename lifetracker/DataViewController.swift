ddcbe13b521c3adc22830eb2e59c0069e0e73da2
Doug Gandle <Doug@1x-vl929-72-19-101-80.wireless.umass.edu>
2016-11-10 16:04:10 -0500

    first commit - tab-based application frame

---
+++ lifetracker/DataViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DataViewController.swift
+//  lifetracker
+//
+//  Created by Doug Gandle on 11/10/16.
+//  Copyright © 2016 Doug Gandle. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
