66844821ccf2a1a68b0e711d9754eba9579ad4bb
Benjamin Danzig <BenjaminDanzig@1x-nat-vl930-172-30-64-250.wireless.umass.edu>
2016-12-12 15:44:52 -0500

    add view controller for edit history page

---
+++ lifetracker/EditHistoryViewController.swift
@@ -0,0 +1,24 @@
+//
+//  EditHistoryViewController.swift
+//  lifetracker
+//
+//  Created by Benjamin Danzig on 12/12/16.
+//  Copyright © 2016 Doug Gandle. All rights reserved.
+//
+import UIKit
+
+class EditHistoryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
